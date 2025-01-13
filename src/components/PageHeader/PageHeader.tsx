d5d6e83f1bb7b5c0a211bbc289bea3992f208f99
André Pereira <andre.pereira.cpc@gmail.com>
2025-01-13 19:57:08 -0100

    feat(todos): add Github/LinkedIn to header; Icons are now standalone components

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,3 +1,5 @@
+import { Github } from "../../icons/Github";
+import { LinkedIn } from "../../icons/LinkedIn";
 import "./PageHeader.css";
 
 export type PageHeaderProps = {
@@ -13,9 +15,28 @@ export const PageHeader = ({ title, background }: PageHeaderProps) => {
         <div className="background-container w-full h-page-header">
           <img className="w-full" src={bg} alt={title} />
         </div>
-        <div className="header-content max-w-screen-2xl mx-auto">
-        <h1 className="absolute bottom-0 ps-5"> {title}</h1>
-
+        <div className="top-0 absolute header-content w-full h-full">
+          <div className="mx-auto max-w-screen-2xl relative h-full">
+            <h1 className="absolute bottom-0 ps-5"> {title}</h1>
+            <div className="absolute top-8 sm:top-auto sm:bottom-6 right-4">
+              <div className="flex items-center">
+                <a
+                  className="text-black"
+                  href="https://github.com/andrehapereira"
+                  target="_blank"
+                >
+                  <Github />
+                </a>
+                <a
+                  className="text-black ms-3"
+                  href="https://www.linkedin.com/in/andrehapereira/"
+                  target="_blank"
+                >
+                  <LinkedIn />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
